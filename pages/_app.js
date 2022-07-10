f31022701b3f9570c797c8a06fccdde93830eb27
danieldoteta <daniel.eta@outlook.com>
2022-07-10 23:07:02 +0100

    initial cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return <>
+          <Head>
+            <meta charset="UTF-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Daniel Eta👋🏾</title>
+          </Head>
+          <Component {...pageProps} />
+         </>
+
 }
 
 export default MyApp
