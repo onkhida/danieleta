661a13e8b26a9be462831fc36b2c632c45b7bb5a
danieldoteta <etadaniel7@gmail.com>
2022-07-14 16:26:09 +0100

    experience and packages section

--- pages/software.js
+++ pages/software.js
@@ -2,6 +2,7 @@ import styles from "../styles/Software.module.css"
 import Project from '../components/Project'
 import Footer from '../components/Footer'
 import Work from '../components/Work'
+import Pill from '../components/Pill'
 import alcg from '../public/alcg.png'
 import dot from '../public/dot.png'
 import commerce from '../public/commerce.png'
@@ -9,9 +10,11 @@ import danieleta from '../public/deta.png'
 import riyadha from '../public/riyadha.png'
 
 export default function Software({ isDark }) {
-    return (
+    const skills = [
+        "electron", "next", "django", "react", "numpy", "redis", "aws", "celery", "s3", "django-channels"
+    ]
 
-        
+    return ( 
         <>
             <div id={`${isDark ? "" : "black-txt"}`} className={styles.topwrap}>
                 <h1 className={styles.title}>Software</h1>
@@ -67,9 +70,55 @@ export default function Software({ isDark }) {
                     
                 </div>
 
-                <div className={styles.skills}>
+                <div id={`${isDark ? "" : "black-txt"}`} className={styles.skills}>
                     <h3>Professional Experience</h3>
+                    <Work 
+                        job="Tech Lead "
+                        location="Cardinal Keys, Nigeria "
+                        separator="at "
+                        url="https://cardinalkeys.com"
+                        description="Software company in Lagos, Nigeria that delivers world-class digital tools and solutions that ensure companies become more efficient, engaging and responsive."
+                        isDark={isDark}
+                    />
 
+                    <Work 
+                        job="Site Technician "
+                        location="OAM, South Africa "
+                        separator="at "
+                        url="https://www.onceamonth.org.za"
+                        description="Non-profit organization committed to empowering underprivileged refugee and asylum-seeking women in South Africa with access to period products and menstrual education."
+                        isDark={isDark}
+                    />
+
+                    <Work 
+                        job="Developer "
+                        location="Dot "
+                        separator="at "
+                        url="https://dotinc.netlify.app"
+                        description="An all-in-one financial service to students on the African Continent"
+                        isDark={isDark}
+                    />
+
+                    <Work 
+                        job="Web Developer "
+                        location="ALCG "
+                        separator="at "
+                        url="https://www.alcg.co.za"
+                        description="An organization that works to aid in the growth of small to medium-size businesses on the African continent through top-notch consulting."
+                        isDark={isDark}
+                    />
+
+                    <h3 id={`${isDark ? "" : "black-txt"}`}>Skills and Packages</h3>
+                    <div className={styles.pills}>
+                        {
+                            skills.map((skill, index) => (
+                                <Pill styles={styles} name={skill} key={index} />
+                            ))
+                        }
+                    </div>
+
+
+                    
 
                 </div>
             </div>
